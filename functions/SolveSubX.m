d58f85f41814194137bd809b40b0acc21c7c3b3f
Avrona <127651428+Mizera-Mondo@users.noreply.github.com>
2023-07-03 19:41:45 +0800

    fix: bug fix on SolveSubX.m, primary test pass

--- functions/SolveSubX.m
+++ functions/SolveSubX.m
@@ -4,8 +4,9 @@ function X = SolveSubX(Y, R, B, L, alpha, k, options)
 % rank(X) <= k
 arguments
     Y, R, B, L, alpha, k double
-    options.rho = 1;
+    options.rho = 100
     options.tol = 1e-3
+    options.maxIter = 2000
     options.debug = false
 end
 tol = options.tol;
@@ -24,17 +25,28 @@ Xi = X - Phi;
 I = eye(n);
 Ix = eye(m);
 DY = D(Y);
-Kz = inv(2*alpha*L + (1 + rho)*I);
+
+if options.debug
+    Larg = @(X, Z, Phi, Theta, Xi)  0.5*(norm(DY - Z, 'fro'))^2 + ...
+          alpha*trace(Z'*L*Z) + trace(Theta'*(Z - X + R*X*B)) + ...
+          rho/2*(norm(Z - X + R*X*B, 'fro'))^2 + trace(Xi'*(X - Phi)) + ...
+          rho/2*(norm(X - Phi, 'fro'))^2;
+end
+
+
 
 isConverge = false;
 isMaxIter = false;
+iter = 1;
 while ~isConverge && ~isMaxIter
     X_old = X;
-
+    if options.debug
+        disp(num2str(Larg(X, Z, Phi, Theta, Xi)));
+    end
     % Update of X
     % The update of X is divided into sub-problems w.r.t. X's rows.
     for i = 1:n
-        z = Z(i, :) + 1/rho*Phi(i, :);
+        z = Z(i, :) + 1/rho*Theta(i, :);
         z = z';
         phi = Phi(i, :) - 1/rho*Xi(i, :);
         phi = phi';
@@ -42,24 +54,38 @@ while ~isConverge && ~isMaxIter
         x = (2*Ix + r^2*B*B' - r*(B + B'))\((Ix - r*B)*z + phi);
         X(i, :) = x';
     end
-
+    if options.debug
+        disp(num2str(Larg(X, Z, Phi, Theta, Xi)));
+    end
     % Update of Z
-    Z = Kz*(DY + rho*D(X) - Theta);
-
+    Z = (2*alpha*L + (1 + rho)*I)\(DY + rho*D(X) - Theta);
+    if options.debug
+        disp(num2str(Larg(X, Z, Phi, Theta, Xi)));
+    end
     % Update of Phi
     Phi = singularValueThrottling(X + Xi/rho, k);
-
+    if options.debug
+        disp(num2str(Larg(X, Z, Phi, Theta, Xi)));
+    end    
     % Update of Theta
     Theta = Theta + rho*(Z - (X - R*X*B));
-
+    if options.debug
+        disp(num2str(Larg(X, Z, Phi, Theta, Xi)));
+    end
     % Update of Xi
     Xi = Xi + rho*(X - Phi);
-
+    if options.debug
+        disp(num2str(Larg(X, Z, Phi, Theta, Xi)));
+        disp("=======================================");
+    end
     % Terminal Condition Check
     primalRes = norm(Phi - X, 'fro')/norm(X, 'fro');
     dualRes = norm(X_old - X, 'fro')/norm(X_old, 'fro');
     isConverge = primalRes <= tol && dualRes <= tol;
+    isMaxIter = iter >= options.maxIter;
+    iter = iter + 1;
 end
 
 end
+
 
