075cfbc10cc8d0543e3b639e0414b27c0eaa239e
Avrona <127651428+Mizera-Mondo@users.noreply.github.com>
2023-07-03 13:36:50 +0800

    feat: SolveSubX.m, row-wise X update scheme

--- functions/SolveSubX.m
+++ functions/SolveSubX.m
@@ -11,27 +11,44 @@ end
 tol = options.tol;
 rho = options.rho;
 
+D = @(X) X - R*X*B;
+
 X = Y;
 Z = D(X);
 Phi = X;
 Theta = Z - (X + R*X*B);
 Xi = X - Phi;
-D = @(X) X - R*X*B;
+
+I = eye(size(L))
+DY = D(Y);
+Kz = inv(2*alpha*L + (1 + rho)*L);
 
 isConverge = false;
 isMaxIter = false;
 while ~isConverge && ~isMaxIter
     X_old = X;
-    % Update of X
-    "TODO: SOLVE X USE LYAP
-    % Update of Z ?
     
+    % Update of X
+    % The update of X is divided into sub-problems w.r.t. X's rows.
+    for i = 1:n
+        z = Z(i, :) + 1/rho*Phi(i, :);
+        phi = Phi(i, :) - 1/rho*Xi(i, :);
+        r = R(i, i);
+        X(i, :) = (2*I + r^2*B*B' - r*(B + B'))\((I - r*B)*z + phi);
+    end
+
+    % Update of Z
+    Z = Kz*(DY + rho*D(X) - Theta);
+
     % Update of Phi
     Phi = singularValueThrottling(X + Xi/rho);
+
     % Update of Theta
     Theta = Theta + rho*(Z - (X - R*X*B));
+
     % Update of Xi
     Xi = Xi + rho*(X - Phi);
+
     % Terminal Condition Check
     primalRes = norm(Phi - X, 'fro')/norm(X, 'fro');
     dualRes = norm(X_old - X, 'fro')/norm(X_old, 'fro');
