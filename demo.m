ef1bc217ca2bef272f042d1ccd12bfefbbacd1df
Avrona <127651428+Mizera-Mondo@users.noreply.github.com>
2023-07-12 15:49:38 +0800

    feat: improved demo and a persuasive test data

--- demo.m
+++ demo.m
@@ -7,7 +7,7 @@ usedEigNum = 9;
 signalLength = 1000;
 noiseCov = 0.1;
 rPertubation = 0.01;
-
+threA = 1e-3;
 % Signal Generation
 [Y, A, R] = genRandomSignal(nodeNum, usedEigNum, signalLength, noiseCov, rPertubation);
 
@@ -16,17 +16,27 @@ B = zeros(signalLength);
 B(2:end, 2:end) = eye(signalLength - 1);
 D = @(X) X - R*X*B;
 alpha = 0.1;
-beta = 225;
+beta = 200;
 
 % Estimation
 [X, Lest] = GL_LRT(Y, R, usedEigNum, alpha = alpha, beta = beta);
 
 % Results
 errLap = norm(Lest - L, 'fro')/norm(L, 'fro');
+Aest = diag(diag(Lest)) - Lest;
 disp("============================================");
 disp("Estimation finished. NMSE of Laplacian: " + num2str(100*errLap) + "%");
+[a, r, p, fM] = classifierPerformance(A > threA, Aest > threA);
+disp("Accuracy  Recall   Precision   f-Measure");
+disp(num2str([a, r, p, fM]));
+% Visualized Results
 close all;
-figure; imagesc(L); colorbar; title('Ground Truth');
-figure; imagesc(Lest); colorbar; title('Estimated');
-figure; [~, S, ~] = svd(Y); imagesc(S(:, 1:nodeNum)); title("Singular Values of Sampled Signals");
-figure; [~, S, ~] = svd(X); imagesc(S(:, 1:nodeNum)); title("Singular Values of Denoised Signals");
+figure;
+subplot(2, 2, 1)
+imagesc(L); colorbar; title('Ground Truth');
+subplot(2, 2, 2)
+imagesc(Lest); colorbar; title('Estimated');
+subplot(2, 2, 3)
+[~, S, ~] = svd(Y); imagesc(S(:, 1:nodeNum)); title("Singular Values of Sampled Signals");
+subplot(2, 2, 4)
+[~, S, ~] = svd(X); imagesc(S(:, 1:nodeNum)); title("Singular Values of Denoised Signals");
